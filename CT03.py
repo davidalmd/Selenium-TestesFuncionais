c88450bf8535da1cc0d9cb297b3f833edeb525cf
cLausx0 <david.almeida2707@gmail.com>
2024-12-14 17:21:09 -0300

    Caso de Teste 03 finalizado

--- CT03.py
+++ CT03.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
@@ -11,6 +12,8 @@ driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 driver.get("http://sga.leiteviana.com:8080")
 
 try:
+
+    # Fazendo Login
     campoUsername = driver.find_element(By.ID, "user")
     campoSenha = driver.find_element(By.ID, "senha")
 
@@ -19,7 +22,45 @@ try:
 
     driver.find_element(By.TAG_NAME, "button").send_keys(Keys.RETURN)
 
-    driver.find_element(By.LINK_TEXT, "Cadastrar Aluno")
+    # Clicando no botão de cadastro de aluno
+    driver.find_element(By.LINK_TEXT, "Cadastrar Aluno").click()
+
+    # Preenchendo os campos de cadastro de aluno
+    campoNome = driver.find_element(By.ID, "nome")
+    campoNome.send_keys("Gabriel Victor")
+
+    campoCurso = Select(driver.find_element(By.ID, "curso"))
+    campoCurso.select_by_index(1)
+
+    # Clicando no botão de gerar matrícula
+    driver.find_element(By.CLASS_NAME, "btn-primary").click()
+
+    campoTurno = Select(driver.find_element(By.ID, "turno"))
+    campoTurno.select_by_index(0)
+
+    campoStatus = Select(driver.find_element(By.ID, "status"))
+    campoStatus.select_by_index(0)
+
+    # Clicando no botão de salvar cadastro do aluno
+    driver.find_element(By.CLASS_NAME, "btn-outline-success").click()
+    
+    # Clicando no botão de editar
+    driver.find_element(By.CLASS_NAME, "btn-primary").click()
+
+    # Editando informações do aluno
+    campoNome = driver.find_element(By.ID, "nome")
+    campoNome.clear()
+    campoNome.send_keys("César Cavalcanti")
+
+    campoCurso = Select(driver.find_element(By.ID, "curso"))
+    campoCurso.select_by_index(2)
+
+    # Clicando no botão de salvar edição do aluno
+    driver.find_element(By.CLASS_NAME, "btn-outline-success").click()
+    
+    # Clicando no botão de excluir aluno
+    driver.find_element(By.CSS_SELECTOR, "a.btn-danger").click()
+
 except:
     print("Algo deu errado com seu teste automatizado.")
 
